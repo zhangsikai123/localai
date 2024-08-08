1dae7ec359ce079cd6d61ecce5c72b1676a22815
sikai zhang <sikai@biomap.com>
2024-08-08 10:54:55 +0800

    upgrade bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,9 +1,23 @@
 import os
+import socket
+from backend.configs.server_config import API_SERVER
 
+def get_local_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))  # Connect to a known external server
+        ip_address = s.getsockname()[0]
+        s.close()
+        return ip_address
+    except socket.error:
+        raise Exception("Unable to get local IP address")
+    
+ip_address = get_local_ip()
+
+port = API_SERVER["port"]
 api_key = input("Enter API_KEY: ")
-ip_address = input("Enter the IP address to expose: ")
 os.environ["OPENAI_API_KEY"] = api_key
-os.environ["VITE_BASE_URL"] = f"http://{ip_address}:7861"
+os.environ["VITE_BASE_URL"] = f"http://{ip_address}:{port}"
 
 # Output the set environment variable values
 print("Environment variables set:")
