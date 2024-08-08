47feb481012a9678f3a07f4b9b5a29ed757f5809
sikai zhang <sikai@biomap.com>
2024-08-08 10:35:02 +0800

    fix migrate.py

--- backend/migrate.py
+++ backend/migrate.py
@@ -1,10 +1,13 @@
 from server.db.base import Base
 from server.db.base import engine
+from server.db.models import UserModel
+from server.db.models import MessageModel
+from server.db.models import ThreadModel
 
+__all__ = ["UserModel", "MessageModel", "ThreadModel"]
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
-
 
 create_tables()
 # add user admin
